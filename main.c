00f6d83342d330fe49a0052d596ec7d9a24c4564
davitmartirosyan <phpencoding@gmail.com>
2023-04-08 04:44:18 +0400

    cub3d day6

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tumolabs <tumolabs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 23:49:41 by dmartiro          #+#    #+#             */
-/*   Updated: 2023/04/07 14:14:31 by dmartiro         ###   ########.fr       */
+/*   Updated: 2023/04/08 04:42:03 by tumolabs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,5 @@ int main(int ac, char **av)
         // run(table);
     }
 }
+
+//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
